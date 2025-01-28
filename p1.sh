2b8a5573be136403fe75d6ceb77498fa46c1b2c8
CHRIS ALEX <159701156+Chris42alex@users.noreply.github.com>
2025-01-28 16:23:40 +0530

    Add files via upload

---
+++ p1.sh
@@ -0,0 +1,15 @@
+echo "Enter your name"
+read name
+echo "Enter you semester"
+read sem
+echo "Enter your batch"
+read batch
+echo "Enter your roll no."
+read roll
+echo "Enter your register no."
+read reg
+echo "Name :"$name;
+echo "Semester :"$sem;
+echo "Batch :"$batch;
+echo "Roll Number :"$roll;
+echo "Registered Number :"$reg;
---
+++ p3.sh
@@ -0,0 +1,11 @@
+echo "Enter 2 numbers"
+read a
+read b
+if(($a>$b))
+then
+echo "largest number is"$a
+else
+echo "largest number is"$b
+fi
+
+
